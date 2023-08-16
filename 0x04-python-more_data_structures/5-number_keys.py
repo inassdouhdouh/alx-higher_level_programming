8e9f3334b490b396f02bae4c4eae061fbabed7cf
inass <douhdouhinas14@gmail.com>
2023-08-16 07:02:10 -0700

    .

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-# 5-number_keys.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def number_keys(a_dictionary):
-    """Return the number of keys in a dictionary."""
-    return (len(a_dictionary))
-
+    no_keys = len(a_dictionary)
+    return no_keys
